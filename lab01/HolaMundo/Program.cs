727e883dbf1c9ac8d595d06f10ce60f7e23c7749
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2021-08-22 19:56:02 -0500

    lab01 -parte 1

---
+++ lab01/HolaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, como te llamas :");
+
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Mucho gusto en conocerte " + nombre);
+        }
+    }
+}
