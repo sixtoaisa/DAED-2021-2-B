5d7c5f9aeee2207776a6c34026ddf6433ad3cd7d
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2021-09-11 09:57:18 -0500

    CONEXION A LA BD DB_LAB03 Y VALIDACION DE LOGIN POR MEDIO DE UNA  BD EN FORMULARIO LOGIN LAB02

--- lab02/Lab02-01/frmLogin.cs
+++ lab02/Lab02-01/frmLogin.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Lab02_01
 {
     public partial class frmLogin : Form
     {
+
+        SqlConnection conn;
+
         public frmLogin()
         {
             InitializeComponent();
         }
+
+        
 
         private void label3_Click(object sender, EventArgs e)
         {
@@ -24,6 +31,7 @@ namespace Lab02_01
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
+           
 
             if (validarUsuario(txtUsuario.Text,txtPassword.Text))
             {
@@ -40,13 +48,53 @@ namespace Lab02_01
            
         }
 
-        private Boolean validarUsuario(String login,String clave)
+        private void conectar()
         {
-            bool retorno = false;
-            if (login=="demo" && clave=="demo")
+            String bd = "db_lab03";
+            String user = "sa";
+            String pwd = "123456";
+            String servidor = "DESKTOP-FK8R0OS\\LOCAL";
+            String str = "Server=" + servidor + ";DataBase=" + bd + ";User Id=" + user + ";Password=" + pwd + ";";
+
+            conn = new SqlConnection(str);
+            conn.Open();
+        }
+
+        private void desconectar()
+        {
+            if (this.conn.State!=ConnectionState.Closed)
             {
-                retorno= true;
+                conn.Close();
             }
+        }
+
+        private Boolean validarUsuario(String usuario,String clave)
+        {
+            conectar();
+
+            String sql = "SELECT * FROM tbl_usuario WHERE usuario_nombre=@Usuario and usuario_password=@Password ";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            SqlParameter param1 = new SqlParameter();
+            param1.ParameterName = "@Usuario";
+            param1.Value = usuario;
+
+            SqlParameter param2 = new SqlParameter();
+            param2.ParameterName = "@Password";
+            param2.Value = clave;
+
+            cmd.Parameters.Add(param1);
+            cmd.Parameters.Add(param2);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            bool retorno = false;
+            while (reader.Read())
+            {
+                retorno = true;
+            }
+            desconectar();
+
             return retorno;
         }
 
