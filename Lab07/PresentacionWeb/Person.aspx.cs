4e0459bc59049e3b1dc589dcd10c2f468541f4af
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2021-10-20 11:17:56 -0500

    Ejercicio 1, Ejercicio 2 y Ejercicio 3 - Se agrego una capa web a nuestra aplicación de capas lab 07

---
+++ Lab07/PresentacionWeb/Person.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using System.Data;
+
+namespace PresentacionWeb
+{
+    public partial class Person : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            ClsNegPerson np = new ClsNegPerson();
+            dt = np.GetAll();
+
+            dgDatos.DataSource = dt;
+            dgDatos.DataBind();
+        }
+
+        protected void dgDatos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
