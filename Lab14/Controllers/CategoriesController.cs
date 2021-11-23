5b2ea2c1452149fc9d56fe19d2bd5f953fd9e0c4
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2021-11-23 17:01:56 -0500

    Laboratorio terminado CRUD para Categories y Products

---
+++ Lab14/Controllers/CategoriesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab14.Models;
+
+namespace Lab14.Controllers
+{
+    public class CategoriesController : Controller
+    {      
+        #region Contexto
+        //ATRIBUTO CONTEXTO
+        private NORTHWNDEntities1 _contexto;
+
+        //PROPIEDAD CONTEXTO
+        public NORTHWNDEntities1 Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                {
+                    _contexto = new NORTHWNDEntities1();
+                }
+                return _contexto;
+            }
+        }
+        #endregion
+
+        //GET: CATEGORIES
+        public ActionResult Index()
+        {
+            return View(Contexto.Categories.ToList());
+        }
+
+        //GET : /CATEGORIES/DETAILS/5
+        public ActionResult Details(int id)
+        {
+
+            var productosPorCategoria = from p in Contexto.Products
+                                        orderby p.ProductName ascending
+
+                                        where p.CategoryID == id
+                                        select p;
+            return View(productosPorCategoria.ToList());
+        }
+
+        // GET: /CATEGORIES/CREATE
+        //MUESTRA EL FORMULARIO
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: /CATEGORIES/CREATE
+        //REGISTRA NUEVA CATEGORIA EN LA BD
+        [HttpPost]
+        public ActionResult Create(Categories nuevaCategoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Categories.Add(nuevaCategoria);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                //MUESTRA LA VISTA CREATE CON DATOS INGRESADOS
+                return View(nuevaCategoria);
+            }
+            catch
+            {
+                //MUESTRA LA VISTA CREATE VACIA
+                return View();
+            }
+        }
+
+        // GET: /CATEGORIES/EDIT/5
+        //MUESTRA FORMULARIO PARA EDICION
+        public ActionResult Edit(int? id)
+        {
+            //SI EL ID ES NULO
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Categories categoriaEditar = Contexto.Categories.Find(id);
+
+            if (categoriaEditar == null)
+                return HttpNotFound();
+            //ENVIA LA CATEGORIA A EDITAR A LA VISTA
+            return View(categoriaEditar);
+        }
+
+        //POST: /CATEGORIES/EDIT/5
+        //REGISTRAR CAMBIOS DE LA CATEGORIA EN LA BD
+        [HttpPost]
+        public ActionResult Edit(Categories CategoriaEditar)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    Contexto.Entry(CategoriaEditar).State = EntityState.Modified;
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                //MUESTRA LA VISTA EDIT CON LOS DATOS INGRESADOS
+                return View(CategoriaEditar);
+            }
+            catch(Exception e)
+            {
+                
+                //MUESTRA LA VISTA EDIT VACIA
+                return View();
+            }
+        }
+
+        // GET : /CATEGORIES/DELETE/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Categories categoriaEliminar = Contexto.Categories.Find(id);
+
+            if (categoriaEliminar == null)
+                return HttpNotFound();
+
+            return View(categoriaEliminar);
+        }
+
+        // POST: /CATEGORIES/DELETE/5
+        [HttpPost]
+        public ActionResult Delete(int? id, Categories categoria1)
+        {
+            try
+            {
+                Categories categoriaEliminar = new Categories();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                    categoriaEliminar = Contexto.Categories.Find(id);
+
+                    //SI NO ENCUENTRA LA CATEGORIA MUESTRA MENSAJE
+                    if (categoriaEliminar == null)
+                        return HttpNotFound();
+
+                    Contexto.Categories.Remove(categoriaEliminar);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                return View(categoriaEliminar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab14/Controllers/ProductsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab14.Models;
+
+namespace Lab14.Controllers
+{
+    public class ProductsController : Controller
+    {
+        #region Contexto
+        //ATRIBUTO CONTEXTO
+        private NORTHWNDEntities1 _contexto;
+
+        //PROPIEDAD CONTEXTO
+        public NORTHWNDEntities1 Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                {
+                    _contexto = new NORTHWNDEntities1();
+                }
+                return _contexto;
+            }
+        }
+        #endregion
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /PRODUCTS/CREATE
+        //MUESTRA EL FORMULARIO
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: /PRODUCTS/CREATE
+        //REGISTRA NUEVO PRODUCTO EN LA BD
+        [HttpPost]
+        public ActionResult Create(Products nuevoProducto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Products.Add(nuevoProducto);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Details", "Categories", new { id = nuevoProducto.CategoryID });
+                }
+                //MUESTRA LA VISTA CREATE CON DATOS INGRESADOS
+
+
+               
+                return View(nuevoProducto);
+            }
+            catch
+            {
+                //MUESTRA LA VISTA CREATE VACIA
+                return View();
+            }
+        }
+
+        // GET: /PRODUCTS/EDIT/5
+        //MUESTRA FORMULARIO PARA EDICION
+        public ActionResult Edit(int? id)
+        {
+            //SI EL ID ES NULO
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Products productoEditar = Contexto.Products.Find(id);
+
+            if (productoEditar == null)
+                return HttpNotFound();
+            //ENVIA LA CATEGORIA A EDITAR A LA VISTA
+            return View(productoEditar);
+        }
+
+        //POST: /PRODUCTS/EDIT/5
+        //REGISTRAR CAMBIOS DEL PRODUCTO EN LA BD
+        [HttpPost]
+        public ActionResult Edit(Products productoEditar)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    Contexto.Entry(productoEditar).State = EntityState.Modified;
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Details", "Categories", new { id = productoEditar.CategoryID });
+                }
+                //MUESTRA LA VISTA EDIT CON LOS DATOS INGRESADOS
+                return View(productoEditar);
+            }
+            catch (Exception e)
+            {
+
+                //MUESTRA LA VISTA EDIT VACIA
+                return View();
+            }
+        }
+
+        // GET : /PRODUCTS/DELETE/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Products productoEliminar = Contexto.Products.Find(id);
+
+            if (productoEliminar == null)
+                return HttpNotFound();
+
+            return View(productoEliminar);
+        }
+
+        // POST: /PRODUCTS/DELETE/5
+        [HttpPost]
+        public ActionResult Delete(int? id,Products producto1)
+        {
+            try
+            {
+                Products productoEliminar = new Products();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                    productoEliminar = Contexto.Products.Find(id);
+                   
+
+                    //SI NO ENCUENTRA EL PRODUCTO MUESTRA MENSAJE
+                    if (productoEliminar == null)
+                        return HttpNotFound();
+
+                    Contexto.Products.Remove(productoEliminar);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Details", "Categories", new { id = productoEliminar.CategoryID });
+                }
+                return View(productoEliminar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
